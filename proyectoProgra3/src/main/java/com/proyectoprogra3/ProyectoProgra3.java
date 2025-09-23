6d94633eb7c4ec98c490289ce909059d1393d48b
JoelERF <joelxr55@gmail.com>
2025-09-23 10:43:38 -0700

    primera clase creada
    solo cree la clase, aun no tiene nada

---
+++ proyectoProgra3/src/main/java/com/proyectoprogra3/ProyectoProgra3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyectoprogra3;
+
+/**
+ *
+ * @author le0jx
+ */
+public class ProyectoProgra3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
